90e825b5db9183e5c5ae2c2297ea57e3b1589ab9
laplamgor <laplamgor@gmail.com>
2019-10-15 23:58:15 +0800

    Fix HTML5 audio by pretending to be iOS

--- app/src/main/java/com/antest1/kcanotify/h5/GameOOIActivity.java
+++ app/src/main/java/com/antest1/kcanotify/h5/GameOOIActivity.java
@@ -85,7 +85,10 @@ public class GameOOIActivity extends XWalkActivity {
     XWalkView mWebview;
     XWalkSettings mWebSettings;
     private ProgressBar progressBar1;
-    private final static String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
+
+    // HTML5 audio is bugged in Crosswalk
+    // Use iPhone UA so that Kancolle will fallback to traditional audio playing
+    private final static String USER_AGENT = "Mozilla/5.0 (iPhone) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
 
     private WebviewBroadcastReceiver webviewBroadcastReceiver = new WebviewBroadcastReceiver();
 
